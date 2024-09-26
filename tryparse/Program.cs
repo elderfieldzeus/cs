891b6adea28d00c45a2b8a229f6691a11f51d839
elderfieldzeus <elderfieldzeus24@gmail.com>
2024-09-26 22:22:09 +0800

    feat: added tryparse

---
+++ tryparse/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program {
+    public static void Main(string[] args) {
+        Console.Write("Enter number: ");
+        
+        int num;
+
+        if(!int.TryParse(Console.ReadLine(), out num)) {
+            Console.WriteLine("Failure to get number");
+        }
+        else {
+            Console.WriteLine($"Your number is {num}...");
+        }
+    }
+}
